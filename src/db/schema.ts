8d937d51d7cc61ab9fdf8c48d5d58c340dc3201e
Karl Edenstål <karl.edenstal@rhyme.se>
2024-02-02 12:49:37 +0100

    updat

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { blob, integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export type UserAddress = {
   street: string;
--- src/users/route.ts
+++ src/users/route.ts
@@ -1,9 +1,30 @@
 import { Elysia } from "elysia";
 import { ID_TYPES, user_schema, users_schema } from "./types";
-import { generateUser } from "./func";
 import { db } from "../db/db";
 import { users_table } from "../db/schema";
 import { count } from "drizzle-orm";
+
+function identifyIdType(
+  id: string | number
+): "id" | "uuid" | "objectid" | undefined {
+  // Check if it's a number type
+  if (typeof id === "number" || !isNaN(Number(id))) {
+    return "id";
+  }
+
+  // MongoDB ObjectId is a 24-character hex string
+  const objectIdRegex = /^[0-9a-fA-F]{24}$/;
+  if (objectIdRegex.test(id)) {
+    return "objectid";
+  }
+
+  // UUID v4 regex (you can adjust the regex if you want to be more specific about the UUID version)
+  const uuidV4Regex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  if (uuidV4Regex.test(id)) {
+    return "uuid";
+  }
+}
 
 export const user_route = new Elysia({ prefix: "/users" })
   .get(
@@ -43,8 +64,43 @@ export const user_route = new Elysia({ prefix: "/users" })
   )
   .get(
     "/:id",
-    ({ params: { id } }) => {
-      return generateUser(id);
+    async ({ params: { id } }) => {
+      try {
+        const typeOfId = identifyIdType(id);
+        let val: string | number = id;
+
+        if (typeOfId == null) {
+          throw new Error("Invalid ID");
+        }
+
+        if (typeOfId === "id") {
+          val = id as number;
+        } else if (typeOfId === "uuid") {
+          val = id.toString();
+        } else if (typeOfId === "objectid") {
+          val = id.toString();
+        }
+
+        const users = await db.select().from(users_table);
+        const user = users.find(
+          (u) =>
+            u.id === +id ||
+            u.uuid === id.toString() ||
+            u.objectid === id.toString()
+        );
+
+        console.log('user', user)
+
+        if (user == null) {
+          throw new Error("User not found");
+        }
+
+        const { id: orgId, uuid, objectid, ...rest } = user;
+
+        return { id: val, ...rest };
+      } catch (e) {
+        console.error(e);
+      }
     },
     user_schema
   );
